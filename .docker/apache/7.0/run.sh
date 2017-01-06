825c857d00786f11a5b2a00dca1882d85bf4f6a7
Tess Flynn <tess@ten7.com>
2017-01-06 11:23:20 -0600

    Added opcache, xdebug configuration.

--- .docker/apache/7.0/run.sh
+++ .docker/apache/7.0/run.sh
@@ -3,6 +3,26 @@
 mkdir -p /run/apache2
 mkdir -p /var/www/html
 
+if [ -n "$PHP_SENDMAIL_PATH" ]; then
+     sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /etc/php7/php.ini
+fi
+
+if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
+     sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php7/conf.d/xdebug.ini
+fi
+
+if [ "$PHP_XDEBUG_AUTOSTART" -eq "0" ]; then
+     sed -i 's/^xdebug.remote_autostart.*/xdebug.remote_autostart = 0/' /etc/php7/conf.d/xdebug.ini
+fi
+
+if [ "$PHP_XDEBUG_REMOTE_CONNECT_BACK" -eq "0" ]; then
+     sed -i 's/^xdebug.remote_connect_back.*/xdebug.remote_connect_back = 0/' /etc/php7/conf.d/xdebug.ini
+fi
+
+if [ -n "$PHP_XDEBUG_REMOTE_HOST" ]; then
+     sed -i 's/^xdebug.remote_host.*/'"xdebug.remote_host = ${PHP_XDEBUG_REMOTE_HOST}"'/' /etc/php7/conf.d/xdebug.ini
+fi
+
 echo "Clearing any old processes..."
 rm -f /run/apache2/apache2.pid
 echo "Starting apache..."
