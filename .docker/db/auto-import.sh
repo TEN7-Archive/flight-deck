b13dbfd4848f0a552b5025c23b59032f23932107
Tess Flynn <tess@ten7.com>
2017-01-18 15:44:02 -0600

    Added compressed file support for DB auto-import.

--- .docker/db/auto-import.sh
+++ .docker/db/auto-import.sh
@@ -3,9 +3,30 @@
 CWD=$( pwd )
 
 cd /tmp/db-backups
-LATEST_DUMP=`ls -td *sql | head -1`
+LATEST_DUMP=`ls -td *sql* | head -1`
 echo $LATEST_DUMP
 
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -C $MYSQL_DATABASE < $LATEST_DUMP;
+case "$LATEST_DUMP" in
+    *.gz)
+          echo Importing gzip compressed file $LATEST_DUMP
+          zcat $LATEST_DUMP | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -C $MYSQL_DATABASE
+          ;;
+    *.xz)
+          echo Importing xz compressed file $LATEST_DUMP
+          xzcat $LATEST_DUMP | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -C $MYSQL_DATABASE
+          ;;
+    *.lzo)
+          echo Importing lzo compressed file $LATEST_DUMP
+          lzopcat $LATEST_DUMP | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -C $MYSQL_DATABASE
+          ;;
+    *.sql)
+          echo Importing uncomressed sql file $LATEST_DUMP
+          mysql --user=root --password=$MYSQL_ROOT_PASSWORD -C $MYSQL_DATABASE < $LATEST_DUMP;
+          ;;
+    *.mysql)
+          echo Importing uncomressed mysql file $LATEST_DUMP
+          mysql --user=root --password=$MYSQL_ROOT_PASSWORD -C $MYSQL_DATABASE < $LATEST_DUMP;
+          ;;
+esac
 
 cd $CWD
