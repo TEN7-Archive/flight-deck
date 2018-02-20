932bfd1093769f7dd983d121a2868073d0615b68
socketwench <tess@deninet.com>
2018-02-20 13:35:55 -0600

    Added secondary storage support to Varnish.

--- .docker/varnish/4.1/varnish-foreground.sh
+++ .docker/varnish/4.1/varnish-foreground.sh
@@ -6,10 +6,12 @@ SCRIPT_NAME=`basename "$0"`
 echo "$SCRIPT_NAME: Starting Varnish in the foreground..."
 
 VARNISH_MEMORY_SIZE=${VARNISH_MEMORY_SIZE:-256m}
+VARNISH_STORAGE_SIZE=${VARNISH_STORAGE_SIZE:-1024m}
 
 varnishd -F \
   -a :6081 \
   -T :6082 \
   -f /etc/varnish/default.vcl \
   -S /etc/varnish/secret \
-  -s malloc,$VARNISH_MEMORY_SIZE
+  -s malloc,$VARNISH_MEMORY_SIZE \
+  -s secondary=file,/var/lib/varnish/storage.bin,$VARNISH_STORAGE_SIZE
