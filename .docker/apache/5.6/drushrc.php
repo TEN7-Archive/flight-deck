cbe59443874349251f8155a315917165af3464be
socketwench <tess@deninet.com>
2018-07-30 13:49:23 -0500

    Set drush root and default shell directory based on APACHE_DOCROOT_DIR.

--- .docker/apache/5.6/drushrc.php
+++ .docker/apache/5.6/drushrc.php
@@ -4,7 +4,10 @@
  *
  * Additional Drush startup customization.
  */
+
+$apache_docroot_dir = getenv('APACHE_DOCROOT_DIR');
+
 if (file_exists('/.dockerenv')) {
   $options['uri'] = 'http://docker.test';
-  $options['root'] = '/var/www/html';
+  $options['root'] = empty($apache_docroot_dir) ? '/var/www/html' : $apache_docroot_dir;
 }
